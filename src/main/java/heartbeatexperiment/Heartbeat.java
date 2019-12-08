4af2864afec63b7cc59f7f9251fe891847dd8aa0
Nick Ayers <n1ckay3r5@gmail.com>
2019-12-08 12:51:19 +0000

    add heartbeat

---
+++ src/main/java/heartbeatexperiment/Heartbeat.java
@@ -0,0 +1,39 @@
+package heartbeatexperiment;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+
+public class Heartbeat {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
+        DataStream<String> stuff = env.socketTextStream("localhost", 8888, "\n");
+        DataStream<String> keyedStuff = stuff.keyBy(it -> it);
+        DataStream<String> stuffWithHearbeat = keyedStuff.process(new ProcessFunction<String, String>() {
+            @Override
+            public void processElement(String value, Context ctx, Collector<String> out) throws Exception {
+                ctx.timerService().registerProcessingTimeTimer(ctx.timerService().currentProcessingTime()+3000);
+                out.collect(value);
+            }
+
+            @Override
+            public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> collector) {
+                collector.collect(String.format("HEARTBEAT %d", ctx.timestamp()));
+                ctx.timerService().registerProcessingTimeTimer(ctx.timerService().currentProcessingTime()+3000);
+            }
+        }).setParallelism(1);
+
+        stuffWithHearbeat.print();
+
+        try {
+            env.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
